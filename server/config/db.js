5eef900165aafb24aece08c5fe83e35dae36bfd4
John Luis Nathaniel Mercado <johnluismercado863@gmail.com>
2025-11-08 22:36:21 +0800

    Update db.js to use environment variables

--- server/config/db.js
+++ server/config/db.js
@@ -3,11 +3,11 @@ const { Pool } = pkg;
 
 // PostgreSQL connection setup
 const pool = new Pool({
-  user: 'postgres',           // your PostgreSQL username
-  host: 'localhost',
-  database: 'tdhi_web',       // your database name
-  password: '1414',  // replace with your PostgreSQL password
-  port: 5432,
+  user: process.env.DB_USER,           // your PostgreSQL username
+  host: process.env.DB_HOST,       // your PostgreSQL host
+  database: process.env.DB_DATABASE,       // your database name
+  password: process.env.DB_PASSWORD, 
+  port: process.env.DB_PORT,
 });
 
 // Test the connection
